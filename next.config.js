c80f049c430d299431d8770346739a1c7bc55ead
Islam <isxmad@gmail.com>
2023-03-15 09:40:17 +0300

    disable export build

--- next.config.js
+++ next.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone', //ftp
+  // output: 'standalone', //ftp
   reactStrictMode: true,
   compiler: {
     styledComponents: true
   },
-  images: { //ftp
-    unoptimized: true
-  }
+  // images: { //ftp
+  //   unoptimized: true
+  // }
 }
 
 module.exports = nextConfig
