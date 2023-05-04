0f6b53623726bb6a3c4d5e318b8ff2dc7a187b22
Islam Madaminov <isxmad@gmail.com>
2023-05-04 22:33:32 +0300

    test dev

--- pageModules/search/components/searchDrawer/SearchDrawer.tsx
+++ pageModules/search/components/searchDrawer/SearchDrawer.tsx
@@ -12,16 +12,6 @@ import { searchFilterType } from '../searchFilter/types';
 
 
 
-const  usePrevious = (value: boolean) => {
-    const previousValueRef = useRef<any>();
-  
-    useEffect(() => {
-      previousValueRef.current = value;
-    }, [value]);
-  
-    return previousValueRef.current;
-}
-
 interface I extends searchFilterType {
     isOpen: boolean,
     onClose: (...args: any[]) => any
@@ -64,137 +54,105 @@ const SearchDrawer:FC<I> = ({
     onToggleDrawer
 }) => {
 
-    const prevIsOpen = usePrevious(isOpen)
-    const controls = useAnimation()
 
-
-
-    const onDragEnd = (e: any, info: any) => {
-        const shouldClose = info.velocity.y > 20 || (info.velocity.y >= 0 && info.point.y > 45);
-
-   
-        if (shouldClose) {
-            controls.start("hidden");
-            onClose();
-        } else {
-            controls.start("visible");
-            onOpen();
+    const onLayerClick = (e: any) => {
+        if(e.target.hasAttribute('layer')) {
+            onClose
         }
     }
 
-    useEffect(() => {
-        if (prevIsOpen && !isOpen) {
-          controls.start("hidden");
-        } else if (!prevIsOpen && isOpen) {
-          controls.start("visible");
-        }
-      }, [controls, isOpen, prevIsOpen]);
 
     return (
-        <motion.div 
-            drag='y'
-            onDragEnd={onDragEnd}
-            initial="hidden"
-            animate={controls}
-            transition={{
-            type: "spring",
-            damping: 40,
-            stiffness: 400
-            }}
-            dragConstraints={{ top: 0 }}
-            dragElastic={0.2}
-            variants={{
-            visible: { y: 0 },
-            hidden: { y: "100%" }
-            }}
-            className={styles.wrapper}>
-            <div className={styles.head}>
-                <div className={styles.title}>Фильтр</div>
-                <div className={styles.close}>
-                    <IconButton
-                        onClick={onClose}
-                        size={15}
-                        icon={<GrClose/>}
-                        variant={'transparent'}
-                        />
+        <div onClick={onLayerClick} className={`${styles.wrapper} ${isOpen ? styles.active : ''}`}>
+            <div className={styles.in}>
+                <div className={styles.head}>
+                    <div className={styles.title}>Фильтр</div>
+                    <div className={styles.close}>
+                        <IconButton
+                            onClick={onClose}
+                            size={15}
+                            icon={<GrClose/>}
+                            variant={'transparent'}
+                            />
+                    </div>
                 </div>
-            </div>
-            <div className={styles.main}>
-                <div className={styles.body}>
-                    <Row gutter={[10,10]}>
-                        <Col span={12}>
-                            <SelectDef
+                <div className={styles.main}>
+                    <div className={styles.body}>
+                        <Row gutter={[10,10]}>
+                            <Col span={12}>
+                                <SelectDef
 
-                                label='Страна'
-                       
-                                placeholder='Страна'
-                                onChange={(e,v) => {
-                                    setCountry(v)
-                                }}
-                                onClear={clearStates}
-                                list={countries}
-                                />
-                        </Col>
-                        <Col span={12}>
-                            {
-                                states?.length > 0 ? (
-                                    <SelectDef
-                                            label='Город'
-                                  
-                                            onChange={(e, v) => {
-                                                setState(v)
-                                            }}
-                                            placeholder='Город'
-                                            list={states}
-                                            />
-                                ) : null
-                            }
-                        </Col>
+                                    label='Страна'
                         
-                        <Col span={12}>
-                            <RangeSlider
-                                
-                                min={18}
-                                max={70}
-                                onChange={e => {
-                                    setage_range_start && setage_range_start(e[0])
-                                    setage_range_end && setage_range_end(e[1])
-                                }}
-                                range={true}
-                                value={[age_range_start,age_range_end]}    
-                                label={'Возраст'}
-                                // unit={'год'}
-                                />
-                        </Col>
-                    </Row>
-                </div>
-                <div className={styles.action}>
-                    <Row gutter={[10,10]}>
-                        <Col span={24}>
-                            <div className={styles.item}>
-                                <Button
-                                    text='Найти'
-                                    middle
-                                    onClick={() => onSearch && onSearch()}
-                                    load={load}
+                                    placeholder='Страна'
+                                    onChange={(e,v) => {
+                                        setCountry(v)
+                                    }}
+                                    onClear={clearStates}
+                                    list={countries}
                                     />
-                            </div>
-                        </Col>
-                        <Col span={24}>
-                            <div className={styles.item}>
-                                <Button
-                                    onClick={clearFilter}
-                                    text='Очистить фильтр'
-                                    middle
-                                    variant={'bordered'}
+                            </Col>
+                            <Col span={12}>
+                                {
+                                    states?.length > 0 ? (
+                                        <SelectDef
+                                                label='Город'
+                                    
+                                                onChange={(e, v) => {
+                                                    setState(v)
+                                                }}
+                                                placeholder='Город'
+                                                list={states}
+                                                />
+                                    ) : null
+                                }
+                            </Col>
+                            
+                            <Col span={12}>
+                                <RangeSlider
+                                    
+                                    min={18}
+                                    max={70}
+                                    onChange={e => {
+                                        setage_range_start && setage_range_start(e[0])
+                                        setage_range_end && setage_range_end(e[1])
+                                    }}
+                                    range={true}
+                                    value={[age_range_start,age_range_end]}    
+                                    label={'Возраст'}
+                                    // unit={'год'}
                                     />
-                            </div>
-                        </Col>
-                        
-                    </Row>
+                            </Col>
+                        </Row>
+                    </div>
+                    <div className={styles.action}>
+                        <Row gutter={[10,10]}>
+                            <Col span={24}>
+                                <div className={styles.item}>
+                                    <Button
+                                        text='Найти'
+                                        middle
+                                        onClick={() => onSearch && onSearch()}
+                                        load={load}
+                                        />
+                                </div>
+                            </Col>
+                            <Col span={24}>
+                                <div className={styles.item}>
+                                    <Button
+                                        onClick={clearFilter}
+                                        text='Очистить фильтр'
+                                        middle
+                                        variant={'bordered'}
+                                        />
+                                </div>
+                            </Col>
+                            
+                        </Row>
+                    </div>
                 </div>
             </div>
-        </motion.div>  
+        </div>  
     )
 }
 
