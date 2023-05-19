84b15eacae0a9a7e091607516fbb20271ecd31b9
Islam Madaminov <isxmad@gmail.com>
2023-05-19 20:51:06 +0300

    fix mail

--- helpers/sorting.ts
+++ helpers/sorting.ts
@@ -10,7 +10,7 @@ export const sortingChatList = (array: any[]) => {
 
 export const sortingMailChatList = (array: any[]) => {
     if(array?.length > 0) {
-        return array.sort((a,b) => moment(a?.updated_at).valueOf() < moment(b?.updated_at).valueOf() ? 1 : -1)
+        return array.sort((a,b) => moment(a?.created_at).valueOf() < moment(b?.created_at).valueOf() ? 1 : -1)
     } else {
         return array;
     }
