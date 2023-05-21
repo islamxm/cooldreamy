22584f090a7f3c9088ad321d24365ab860761b12
Islam Madaminov <isxmad@gmail.com>
2023-05-21 17:20:56 +0300

    reset signup step

--- pageModules/signup/components/Body/Body.tsx
+++ pageModules/signup/components/Body/Body.tsx
@@ -41,7 +41,7 @@ const Body:FC = () => {
     const router = useRouter()
 
     const dispatch = useAppDispatch()
-    const [currentStep, setCurrentStep] = useState(2)
+    const [currentStep, setCurrentStep] = useState(0)
     const [nextBtn, setNextBtn] = useState(false)
 
     // 1 STEP
