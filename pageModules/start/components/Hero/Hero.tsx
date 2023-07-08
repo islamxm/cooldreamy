f3bb644ff9718d09f179d04055b4cbccb74c90a6
Islam <isxmad@gmail.com>
2023-07-08 23:25:10 +0300

    clouds

--- pageModules/start/components/Hero/Hero.tsx
+++ pageModules/start/components/Hero/Hero.tsx
@@ -13,7 +13,22 @@ import { useAppSelector } from '@/hooks/useTypesRedux';
 import Router from 'next/router';
 import Button from '@/components/Button/Button';
 import LoginModal from '@/components/LoginModal/LoginModal';
-import parse from 'html-react-parser'
+import parse from 'html-react-parser';
+import cloud1 from '@/public/assets/images/hero-cloud-1.svg'
+import cloud2 from '@/public/assets/images/hero-cloud-2.svg'
+import cloud3 from '@/public/assets/images/hero-cloud-3.svg'
+import cloud4 from '@/public/assets/images/hero-cloud-4.svg'
+import cloud5 from '@/public/assets/images/hero-cloud-5.svg'
+import cloud6 from '@/public/assets/images/hero-cloud-6.svg'
+import cloud7 from '@/public/assets/images/hero-cloud-7.svg'
+import cloud8 from '@/public/assets/images/hero-cloud-8.svg'
+import stars from '@/public/assets/images/hero-stars.svg'
+import heart1 from '@/public/assets/images/hero-heart-1.svg';
+import heart2 from '@/public/assets/images/hero-heart-2.svg';
+import arrow1 from '@/public/assets/images/hero-arrow-1.svg';
+import arrow2 from '@/public/assets/images/hero-arrow-2.svg';
+
+
 
 const Hero: FC = ({}) => {
     const [sex, setSex] = useState<'male' | 'female'>()
@@ -72,16 +87,138 @@ const Hero: FC = ({}) => {
                         
                     </div>
                     <div className={styles.motion}>
-                            <div className={styles.clouds}>
-
-                            </div>
+                           
                             <motion.div 
                                 className={styles.phones}
                                 style={{scale}}
                                 >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        repeatDelay: 0
+                                        }} className={`${styles.cloud} ${styles.cloud_1}`}><Image src={cloud1} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2.4,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        }} className={`${styles.cloud} ${styles.cloud_2}`}><Image src={cloud2} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2.4,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        }} className={`${styles.cloud} ${styles.cloud_3}`}><Image src={cloud3} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        repeatDelay: 0
+                                        }} className={`${styles.cloud} ${styles.cloud_4}`}><Image src={cloud4} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2.4,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        }} className={`${styles.cloud} ${styles.cloud_5}`}><Image src={cloud5} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        repeatDelay: 0
+                                        }} className={`${styles.cloud} ${styles.cloud_6}`}><Image src={cloud6} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2.4,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        }} className={`${styles.cloud} ${styles.cloud_7}`}><Image src={cloud7} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        repeatDelay: 0
+                                        }} className={`${styles.cloud} ${styles.cloud_8}`}><Image src={cloud8} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2.4,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        }} className={`${styles.hero_stars}`}><Image src={stars} alt=''/></motion.div >
+                                    <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2.4,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        }} className={`${styles.hero_stars_2}`}><Image src={stars} alt=''/></motion.div >
+                                <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        repeatDelay: 0
+                                        }} className={`${styles.heart_2}`}><Image src={heart2} alt=''/></motion.div >
+                                     <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        repeatDelay: 0
+                                        }} className={`${styles.arrow_1}`}><Image src={arrow1} alt=''/></motion.div >
+                                     <motion.div animate={{
+                                            y: ['0px', '-20px', '0px']
+                                        }}
+                                        transition={{
+                                        duration: 2,
+                                        ease: "easeInOut",
+                                        times: [0, 0.5, 1],
+                                        repeat: Infinity,
+                                        repeatDelay: 0
+                                        }} className={`${styles.arrow_2}`}><Image src={arrow2} alt=''/></motion.div >
                                 <motion.div  
                                     variants={item} 
-                                    className={styles.item}>
+                                    className={`${styles.item} ${styles.item_1}`}>
                                     <motion.div
                                         animate={{
                                             y: ['0px', '-20px', '0px']
@@ -101,7 +238,7 @@ const Hero: FC = ({}) => {
                                 </motion.div>
                                 <motion.div 
                                     variants={item}
-                                    className={styles.item}>
+                                    className={`${styles.item} ${styles.item_2}`}>
                                     <motion.div
                                         animate={{
                                             y: ['0px', '-20px', '0px']
