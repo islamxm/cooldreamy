58bde41489ab077dbd3bc4e3fc7cf7821b0609cf
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2023-07-31 19:44:12 +0300

    Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -113,22 +113,6 @@ function App({ Component, pageProps }: AppProps) {
 						</MainWrapper>
 				</PrivateRoute>
 			</ConfigProvider>
-			<script type="text/javascript" >
-				(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-				m[i].l=1*new Date();
-				for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-				k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-				(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-				ym(94454907, "init", {
-				clickmap:true,
-				trackLinks:true,
-				accurateTrackBounce:true,
-				webvisor:true,
-				ecommerce:"dataLayer"
-			});
-			</script>
-			<noscript><div><img src="https://mc.yandex.ru/watch/94454907" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 		</Provider>
 
 	)
