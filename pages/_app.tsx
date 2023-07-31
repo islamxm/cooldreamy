a3ac116982003f9e9ab178b0270a78933eb86669
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2023-07-31 22:32:34 +0300

    Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,6 +32,33 @@ if(process?.browser) {
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 	console.log('asdasdassa')
+	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+		m[i].l=1*new Date();
+		for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+		k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+	ym(94454907, "init", {
+		clickmap:true,
+		trackLinks:true,
+		accurateTrackBounce:true,
+		webvisor:true,
+		ecommerce:"dataLayer"
+	});
+	var div = document.createElement("div");
+
+// Создаем элемент img и задаем атрибуты
+	var img = document.createElement("img");
+	img.src = "https://mc.yandex.ru/watch/94454907";
+	img.style.position = "absolute";
+	img.style.left = "-9999px";
+	img.alt = "";
+
+// Добавляем img внутрь div
+	div.appendChild(img);
+
+// Добавляем div на страницу
+	document.body.appendChild(div);
 }
 
 
