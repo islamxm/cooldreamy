4b74db4f486ba2fcf423918f845221f7709bb5ec
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2023-07-31 22:56:19 +0300

    Commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -31,20 +31,28 @@ if(process?.browser) {
 	const vh = process?.browser && window.innerHeight / 100;
 	document.documentElement.style.setProperty('--vh', `${vh}px`);
 
-	(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-		m[i].l=1*new Date();
-		for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-		k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-	(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-	ym(94454907, "init", {
-		clickmap:true,
-		trackLinks:true,
-		accurateTrackBounce:true,
-		webvisor:true,
-		ecommerce:"dataLayer"
-	});
 	var div = document.createElement("div");
+	var script = document.createElement('script');
+	script.src = 'https://mc.yandex.ru/metrika/tag.js';
+	script.async = true;
+	script.onload = function () {
+		(window.ym || function () {})(
+			94454907,
+			'init',
+			{
+				clickmap: true,
+				trackLinks: true,
+				accurateTrackBounce: true,
+				webvisor: true,
+				ecommerce: 'dataLayer'
+			}
+		);
+	};
+
+	var existingScript = document.querySelector('script[src="https://mc.yandex.ru/metrika/tag.js"]');
+	if (!existingScript) {
+		document.head.appendChild(script);
+	}
 
 // Создаем элемент img и задаем атрибуты
 	var img = document.createElement("img");
