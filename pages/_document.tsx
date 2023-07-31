b317c22228c1eafcd1339bbca8c63dfb4df1de57
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2023-07-31 23:10:35 +0300

    Commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -59,9 +59,22 @@ export default function Document() {
 
           <script
               dangerouslySetInnerHTML={{
-                  __html: `console.log('asdasd')`
+                  __html: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                       m[i].l=1*new Date();
+                       for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                       k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                       (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+                    
+                       ym(94454907, "init", {
+                            clickmap:true,
+                            trackLinks:true,
+                            accurateTrackBounce:true,
+                            webvisor:true,
+                            ecommerce:"dataLayer"
+                       });`
               }}
           />
+          <noscript><div><img src="https://mc.yandex.ru/watch/94454907" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
       </Head>
       <body>
         <Main />
