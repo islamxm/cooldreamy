d21754bbbe645e0abc294f34fa8bd49aa873875b
AlexeyRyzhakov <ryzhakovalexeynicol@gmail.com>
2023-07-31 23:11:53 +0300

    Commit

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -74,7 +74,11 @@ export default function Document() {
                        });`
               }}
           />
-          <noscript><div><img src="https://mc.yandex.ru/watch/94454907" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+          <noscript
+            dangerouslySetInnerHTML={{
+                __html: `<div><img src="https://mc.yandex.ru/watch/94454907" style="position:absolute; left:-9999px;" alt="" /></div>`
+            }}
+          />
       </Head>
       <body>
         <Main />
