661016ef2602da2d86f03adc60baf450b729a58b
Islam <isxmad@gmail.com>
2023-09-20 17:37:51 +0500

    f

--- components/MainWrapper/MainWrapper.tsx
+++ components/MainWrapper/MainWrapper.tsx
@@ -195,13 +195,13 @@ const MainWrapper = ({
 	}
 
 
-	// useEffect(() => {
-	// 	let tm:any;
-	// 	if(token) {
-	// 		getIndicators()
-	// 		tm = setInterval(getIndicators, 4000)
-	// 	}
-	// }, [token])
+	useEffect(() => {
+		let tm:any;
+		if(token) {
+			getIndicators()
+			tm = setInterval(getIndicators, 4000)
+		}
+	}, [token])
 
 
 	// useEffect(() => {
