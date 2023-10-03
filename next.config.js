dcd32da74964d9ead1691d2ff0c6c2281e60bf61
Islam <isxmad@gmail.com>
2023-10-03 19:14:00 +0500

    f

--- next.config.js
+++ next.config.js
@@ -2,7 +2,8 @@
 const withPWA = require('next-pwa')({
   dest: 'public',
   register: true,
-  disableDevLogs: true
+  disableDevLogs: true,
+  mode: process.env.NODE_ENV
 })
 
 const nextConfig = withPWA({
