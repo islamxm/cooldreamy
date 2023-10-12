9c3b2ff962cce6081e2385a5819551fe8c39f689
Islam <isxmad@gmail.com>
2023-10-12 18:41:35 +0500

    f

--- components/MainWrapper/MainWrapper.tsx
+++ components/MainWrapper/MainWrapper.tsx
@@ -363,19 +363,8 @@ const MainWrapper = ({
 					open={emailEditModal}
 					head='Edit e-mail'
 					/>
-			<Script
-          id='gtm-1'
-					async
-          >
-          {
-            `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-59CJTBH');`
-          }
-        </Script>
 			
+				
         <div className={styles.wrapper}>
 				{children}
 			</div>
