e1a19a929e683d3dc031bee39b0496ec179c408d
Islam Madaminov <isxmad@gmail.com>
2023-11-01 02:56:22 -0700

    fix: f

--- pageModules/chat/components/ChatBody/components/ChatCountAvilableMessages/ChatCountAvilableMessages.tsx
+++ pageModules/chat/components/ChatBody/components/ChatCountAvilableMessages/ChatCountAvilableMessages.tsx
@@ -1,25 +1,22 @@
 import styles from './styles.module.scss';
 import {FC, useEffect, useState} from 'react';
-import {FcInfo} from 'react-icons/fc';
+import {AiOutlineInfoCircle} from 'react-icons/ai';
 import { useAppSelector } from '@/hooks/useTypesRedux';
-import PromptModal from '@/popups/PromptModal/PromptModal';
+import CreditsInfoModal from '../CreditsInfoModal/CreditsInfoModal';
 const ChatCountAvilableMessages:FC = () => {
     const {userData} = useAppSelector(s => s)
     const [modal, setModal] = useState(false)
 
     return (
         <div className={styles.wrapper}>
-            <PromptModal
+            <CreditsInfoModal
                 open={modal}
-                text="When the credits are spent, you'll need to buy a paid subscription"
-                onAccept={() => setModal(false)}
-                onReject={() => setModal(false)}
                 onCancel={() => setModal(false)}
                 />
             <div className={styles.badge}>
                 <div className={styles.label}>Доступно сообщений: {userData?.free_credits ? Math.floor(userData?.free_credits / 3) : 0} </div>
                 <div className={styles.info} onClick={() => setModal(true)}>
-                    <FcInfo/>
+                    <AiOutlineInfoCircle/>
                 </div>
             </div>
         </div>
--- pageModules/chat/components/ChatBody/components/CreditsInfoModal/CreditsInfoModal.tsx
+++ pageModules/chat/components/ChatBody/components/CreditsInfoModal/CreditsInfoModal.tsx
@@ -1,14 +1,50 @@
 import {FC} from 'react'
-import { Modal, ModalFuncProps } from 'antd';
+import { Modal, ModalFuncProps, Row, Col } from 'antd';
+import getClassNames from '@/helpers/getClassNames';
+import styles from './styles.module.scss';
+import Button from '@/components/Button/Button';
+import Router from 'next/router';
 
-
-
-const CreditsInfoModal:FC<ModalFuncProps> = () => {
+const CreditsInfoModal:FC<ModalFuncProps> = (props) => {
+    const {onCancel} = props || {}
     return (
         <Modal
-            
+            {...props}
+            width={485}
+            footer={null}
+            className={getClassNames([styles.wrapper, 'modal'])}
             >
-
+            <Row gutter={[25,25]}>
+                <Col span={24}>
+                    <h3 className={styles.title}>Кредиты</h3>
+                </Col>
+                <Col span={24}>
+                    <div className={styles.text}>
+                        When the credits are spent, you'll need to buy a paid subscription
+                    </div>
+                </Col>
+                <Col span={24}>
+                    <Row gutter={[12,12]}>
+                        <Col span={12}>
+                            <Button
+                                text='Пополнить сейчас'
+                                middle
+                                fill
+                                onClick={() => Router.push(`/deposit-mb?tab=3`)}
+                                />
+                        </Col>
+                        <Col span={12}>
+                            <Button
+                                fill
+                                variant={'bordered'}
+                                text='Закрыть'
+                                onClick={onCancel}
+                                middle
+                                />
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         </Modal>
     )
 }
